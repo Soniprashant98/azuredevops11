589f0887a6badad22c22f6b439e6cbfed011e498
Prashant Soni <soni.prashant27@gmail.com>
2025-08-14 16:19:37 +0530

    new rg added

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "gitam" {
   name     = "gitam"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "JNTU"{
+  name     = "JNTU"
+  location = "centralindia"
+}
